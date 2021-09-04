6ba53b2d23758468cde22a81a312bc0a86420db4
marybarker <marybarker103@gmail.com>
2021-09-04 14:03:04 -0500

    added test for enumerating all mutations for a given quiver

---
+++ tests/calculate_num_triangulations.py
@@ -0,0 +1,26 @@
+import sys
+sys.path.append('../')
+from mutations import *
+
+edges = [[0,1],[1,0],
+         [1,2],[2,1],
+         [0,2],[2,0], 
+         [2,3],[3,2], 
+         [3,3],
+         [3,4],[4,3],
+         [4,4],
+         [4,5],[5,4],
+         [5,5],[5,5],
+         [5,0],[0,5]]
+
+cycles = [(0,1,2),(0,2,1),(3,3,4),(1,2,3,2),(0,5,0,1),(2,3,3)]
+coefs = [1,1,1,1,1,1]
+
+QP = QuiverWithPotential(edges, [cycles, coefs])
+
+# set vertex positions
+QP.positions = [[-1,0], [0,1], [1,0], [1,-1], [0,-2], [-1,-1]]
+
+all_mutations = get_all_mutations_from_quiver(QP)
+for Q in all_mutations:
+    Q.draw()
