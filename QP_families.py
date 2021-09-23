6dc31deacf581bfd7fe9f680a5de7d62b461580f
marybarker <marybarker103@gmail.com>
2021-09-23 10:17:53 -0500

    added 1/6(1,2,3) quiver to pre-defined set so I don't have to keep typing it

--- QP_families.py
+++ QP_families.py
@@ -2,7 +2,6 @@ from mutations import *
 import numpy as np
 
 def D2n(n):
-
     # if n is odd
     if n%2 > 0:
         m = int((n-1)/2)
@@ -78,3 +77,22 @@ def cyclicQP(n):
     positions = [(np.sin(angle*i), np.cos(angle*i)) for i in range(n)]
 
     return QuiverWithPotential(edges, potential=[cycles, coefs], positions=positions)
+
+def favorite_example():
+    # this is the 1/6(1,2,3) singularity quiver. It's used so frequently I don't want to keep typing it. 
+    edges = [[0,1],[1,0],
+             [1,2],[2,1],
+             [0,2],[2,0], 
+             [2,3],[3,2], 
+             [3,3],
+             [3,4],[4,3],
+             [4,4],
+             [4,5],[5,4],
+             [5,5],[5,5],
+             [5,0],[0,5]]
+    
+    cycles = [(0,1,2),(0,2,1),(3,3,4),(1,2,3,2),(0,5,0,1),(2,3,3)]
+    coefs = [1,1,1,1,1,1]
+    positions = [[-1,0], [0,1], [1,0], [1,-1], [0,-2], [-1,-1]]
+
+    return QuiverWithPotential(edges, [cycles, coefs], positions=positions)
