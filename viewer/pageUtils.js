cc0b5c5ece35a69aad2110ffa2a582b825086790
marybarker <marybarker103@gmail.com>
2022-08-02 07:42:43 -0700

    making website_version more backwards compatible

---
+++ viewer/pageUtils.js
@@ -0,0 +1,36 @@
+var setButtonsActive = 0;
+
+function openPage(pageName, elmnt) {
+  // Hide all elements with class="tabcontent" by default */
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  // Remove the background color of all tablinks/buttons
+  tablinks = document.getElementsByClassName("tablink");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].style.backgroundColor = "";
+  }
+  // Show the specific tab content
+  document.getElementById(pageName).style.display = "block";
+
+  // check to initialize hidden content displays correctly
+  if (setButtonsActive < 1) {
+    setButtonsActive = 1;
+    var coll = document.getElementsByClassName("collapsible");
+    var i;
+    for (i = 0; i < coll.length; i++) {
+      coll[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        var content = this.nextElementSibling;
+        if (content.style.display === "block") {
+          content.style.display = "none";
+        } else {
+          content.style.display = "block";
+        }
+      });
+    } 
+  }
+}
